7ce4a268c9f1b646ed7a3e2b13fe4813891711e6
David Salcedo <dvsalcedov@gmail.com>
2020-12-27 11:29:13 -0500

    Added first solution

---
+++ DeleteMiddleNode/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DeleteMiddleNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedLists linklist = new LinkedLists();
+            linklist.append(1);
+            linklist.append(2);
+            linklist.append(3);
+            linklist.append(4);
+            linklist.append(5);
+            linklist.append(6);
+            linklist.append(7);
+            linklist.append(8);
+
+            linklist.print();
+            DeleteNode(linklist, 5);          
+            linklist.print();
+        }
+
+        static void  DeleteNode(LinkedLists list, int value)
+        {
+            var node = list.First();
+            while(node != null){
+                if(node.value == value){
+                    node.value = node.next.value;
+                    node.next = node.next.next;
+                    break;
+                }else{
+                    node = node.next;
+                }
+            }
+        }
+    }
+}
